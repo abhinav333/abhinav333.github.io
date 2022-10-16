1508b7b6de44203d65c7aa31f7b29ccec4effb61
abhinav333 <kulkarni.Abhinav91@gmail.com>
2022-10-16 16:36:41 -0400

    new

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,16 @@
 var store = [{
+        "title": "Interfacing SD with AVR: Part 1",
+        "excerpt":"This is the first part in the tutorial series which will guide you to use a SD Card for your projects with AVR ATMEGA. Secure Disk Card (SDC) is a versatile and compact storage peripheral. It is widely used for file storage, owing to its small size and compatibly with...","categories": ["embedded systems"],
+        "tags": [],
+        "url": "/embedded%20systems/2014/09/30/sd-card-1.html",
+        "teaser": null
+      },{
+        "title": "mytest",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","mytest"],
+        "tags": [],
+        "url": "/jekyll/mytest/2022/10/10/mytest.html",
+        "teaser": null
+      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],

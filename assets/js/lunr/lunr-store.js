3a74480d590b524bab1436959e742ef2ba22cbf2
abhinav333 <kulkarni.Abhinav91@gmail.com>
2022-10-17 10:47:00 -0400

    new

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,15 +5,15 @@ var store = [{
         "url": "/embedded%20systems/2014/09/30/sd-card-1.html",
         "teaser": null
       },{
-        "title": "mytest",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","mytest"],
+        "title": "Interfacing SD with AVR: Part 2",
+        "excerpt":"In this part of the tutorial, we will see how SD-Card is wired with ATMEGA16. SD-Card requires a 3.3 V supply, so we will first make a circuit for the same purpose. You can use  voltage regulators like LM117(fixed 3.3 V output) or variable one like the popular LM317. There...","categories": ["embedded systems"],
         "tags": [],
-        "url": "/jekyll/mytest/2022/10/10/mytest.html",
+        "url": "/embedded%20systems/2014/11/12/sd-card-2.html",
         "teaser": null
       },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "Interfacing SD with AVR: Part 3",
+        "excerpt":"Data on storage media  is organised using filesytem. Computer operating system is able to access contents of the storage media via filesystem. We will use Fat Allocation Table (FAT) to format our SD Card. Additional information on FAT file system is available online [1]. To access contents on SDC with...","categories": ["embedded systems"],
         "tags": [],
-        "url": "/jekyll/update/2022/10/16/welcome-to-jekyll.html",
+        "url": "/embedded%20systems/2020/08/28/sd-card-3.html",
         "teaser": null
       }]

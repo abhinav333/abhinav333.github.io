43c257b27885a658ab25c05d2274d76e1e110fda
abhinav333 <kulkarni.ABhinav91@gmail.com>
2023-03-11 15:16:27 -0500

    update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Interfacing SD with AVR: Part 1",
+        "excerpt":"This is the first part in the tutorial series which will guide you to use a SD Card for your projects with AVR ATMEGA. Secure Disk Card (SDC) is a versatile and compact storage peripheral. It is widely used for file storage, owing to its small size and compatibly with...","categories": ["embedded systems"],
+        "tags": [],
+        "url": "/embedded%20systems/2014/09/30/sd-card-1.html",
+        "teaser": null
+      },{
+        "title": "Interfacing SD with AVR: Part 2",
+        "excerpt":"In this part of the tutorial, we will see how SD-Card is wired with ATMEGA16. SD-Card requires a 3.3 V supply, so we will first make a circuit for the same purpose. You can use  voltage regulators like LM117(fixed 3.3 V output) or variable one like the popular LM317. There...","categories": ["embedded systems"],
+        "tags": [],
+        "url": "/embedded%20systems/2014/11/12/sd-card-2.html",
+        "teaser": null
+      },{
+        "title": "Interfacing SD with AVR: Part 3",
+        "excerpt":"Data on storage media  is organised using filesytem. Computer operating system is able to access contents of the storage media via filesystem. We will use Fat Allocation Table (FAT) to format our SD Card. Additional information on FAT file system is available online [1]. To access contents on SDC with...","categories": ["embedded systems"],
+        "tags": [],
+        "url": "/embedded%20systems/2020/08/28/sd-card-3.html",
+        "teaser": null
+      }]

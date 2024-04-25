134884fa8bc5e16d2e9fe9ab8c8d71b9581ed4c6
abhinav333 <kulkarni.ABhinav91@gmail.com>
2024-04-24 20:26:48 -0400

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,19 @@
 var store = [{
-        "title": "Interfacing SD with AVR: Part 1",
+        "title": "Interfacing SD with MCU: Part 1",
         "excerpt":"This is the first part of a tutorial series that will guide you in using an SD Card for your AVR ATMEGA projects. A Secure Digital Card (SDC) is a versatile and compact storage peripheral widely used for file storage due to its small size and compatibility with nearly all...","categories": ["embedded systems"],
-        "tags": [],
+        "tags": ["sd,","mcu"],
         "url": "/embedded%20systems/2014/09/30/sd-card-1.html",
         "teaser": null
       },{
-        "title": "Interfacing SD with AVR: Part 2",
+        "title": "Interfacing SD with MCU: Part 2",
         "excerpt":"In this part of the tutorial, we will explore how to wire an SD Card with an ATMEGA16. The SD Card requires a 3.3 V supply, so we will first create a circuit for this purpose. You can use voltage regulators like the LM117 (fixed 3.3 V output) or a...","categories": ["embedded systems"],
-        "tags": [],
+        "tags": ["sd,","mcu"],
         "url": "/embedded%20systems/2014/11/12/sd-card-2.html",
         "teaser": null
       },{
-        "title": "Interfacing SD with AVR: Part 3",
+        "title": "Interfacing SD with MCU: Part 3",
         "excerpt":"Data on storage media is organized using a filesystem. A computer’s operating system can access the contents of storage media via the filesystem. In this project, we will use the Fat Allocation Table (FAT) to format our SD Card. You can find additional information on the FAT file system online...","categories": ["embedded systems"],
-        "tags": [],
+        "tags": ["sd,","mcu"],
         "url": "/embedded%20systems/2020/08/28/sd-card-3.html",
         "teaser": null
       }]
